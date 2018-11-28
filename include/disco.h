b828efdcb56a5d7a71412b810e7b5b2ad3549515
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-11-27 22:01:26 -0300

    estrutura montada

--- include/disco.h
+++ include/disco.h
@@ -1,7 +1,7 @@
 /**
  * @file disco.h
  * @author Allan de Miranda (allandemiranda@gmail.com)
- * @brief Class Disco
+ * @brief Class disco contendo informações do disco
  * @version 0.1
  * @date 2018-11-27
  * 
@@ -12,22 +12,22 @@
 #ifndef DISCO_H
 #define DISCO_H
 
+#include <vector>
+#include <iterator>
+
 class disco
 {
 private:
-    /* data */
+    std::vector <int> lista_de_espera;  //! Lista de espera para acessar os cilindros do disco
+    int posicao_inicial;                //! Posição inicial do cabeçote
+    int max_cilindro = 199;             //! Posições máxima do cilindro
+    int min_cilindro = 0;               //! Posição mínima do cilindro
 public:
-    disco(/* args */);
-    ~disco();
+    disco(int, std::vector<int>);
+    int tamanho_lista_de_espera(void);
+    std::vector<int>::iterator acessar_elemento_inicial(void);
+    int posicao_cabecote_inicial(void);
+    int distancia(std::vector<int>::iterator, std::vector<int>::iterator);
 };
-
-disco::disco(/* args */)
-{
-}
-
-disco::~disco()
-{
-}
-
 
 #endif
--- src/disco.cpp
+++ src/disco.cpp
@@ -10,3 +10,58 @@
  */
 
 #include "../include/disco.h"
+
+/**
+ * @brief Construct a new disco::disco object
+ * 
+ * @param p_inicial Posição inicial do cabeçote
+ * @param fila Fila de espera para ler cilindro
+ */
+disco::disco(int p_inicial, std::vector <int> fila){
+    posicao_inicial = p_inicial;  //! Posição inicial do cabeçote
+    for(int i : fila){
+        lista_de_espera.push_back(i);   //! Criar fila de espera
+    }
+}
+
+/**
+ * @brief Função para verificar quantos elementos existe na lista de espera
+ * 
+ * @return int Quantidade de elementos na lista de espera
+ */
+int disco::tamanho_lista_de_espera(void){
+    return lista_de_espera.size();
+}
+
+/**
+ * @brief Função para apontar para o início do vetor de espera
+ * 
+ * @return std::vector<int>::iterator Ponteiro para o início
+ */
+std::vector<int>::iterator disco::acessar_elemento_inicial(void){
+    return lista_de_espera.begin();
+}
+
+/**
+ * @brief Verificar posição do cabeçote
+ * 
+ * @return int Retorna posição do cabeçote
+ */
+int disco::posicao_cabecote_inicial(void){
+    return posicao_inicial;
+}
+
+/**
+ * @brief Medir distancia entre dois pontos
+ * 
+ * @param p_inicial Ponteiro inicial
+ * @param p_final Ponteiro final
+ * @return int Distância entre os pontos
+ */
+int disco::distancia(std::vector<int>::iterator p_inicial, std::vector<int>::iterator p_final){
+    int resposta = p_final - p_inicial;
+    if(resposta<0){
+        resposta = resposta * (-1);
+    }
+    return resposta;
+}
